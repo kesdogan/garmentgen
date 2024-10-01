ecddbcb6810a11ca2905e012fe63715148d23069
kesdogan <timurkesdogan@gmail.com>
2024-10-01 01:09:28 -0700

    new rulles for pair generation

--- pattern_sample_pairs.py
+++ pattern_sample_pairs.py
@@ -12,6 +12,7 @@ import random
 import string
 import traceback
 import argparse
+import copy
 
 # Custom
 from pygarment.data_config import Properties
@@ -221,7 +222,8 @@ def generate(path, properties, sys_paths, verbose=False):
         # Redo sampling untill success
         for _ in range(100):  # Putting a limit on re-tries to avoid infinite loops
             new_design = sampler.randomize()
-            name = f'rand_{_id_generator()}'
+            index = _id_generator()
+            name = f'rand_{index}'
             try:
                 if verbose:
                     print(f'{name} saving design params for debug')
@@ -247,24 +249,147 @@ def generate(path, properties, sys_paths, verbose=False):
                 default_body.params['body_sample'] = def_obj_name
 
                 # On random body shape
-                rand_body = body_sample(
-                    body_options,
-                    body_samples_path,
-                    straight=not has_pants(new_design))
-                piece_shaped = MetaGarment(name, rand_body, new_design) 
-                piece_shaped.assert_total_length()   # Check final length correctness
+                # rand_body = body_sample(
+                #     body_options,
+                #     body_samples_path,
+                #     straight=not has_pants(new_design))
+                # piece_shaped = MetaGarment(name, rand_body, new_design) 
+                # piece_shaped.assert_total_length()   # Check final length correctness
                 
-                if piece_default.is_self_intersecting() or piece_shaped.is_self_intersecting():
+                # if piece_default.is_self_intersecting() or piece_shaped.is_self_intersecting():
+                if piece_default.is_self_intersecting():
                     if verbose:
                         print(f'{piece_default.name} is self-intersecting!!') 
                     continue  # Redo the randomization
+                try: 
                 
+                    description = 'no_change'
+                    new_design_pair = copy.deepcopy(new_design)
+                    i = 0
+                    while i < 100:
+                        i += 1
+                        hit = random.randint(0, 5)
+                    
+                        # pants 
+                        if hit == 0 and new_design['meta']['bottom']['v'] == 'Pants':
+                            length = new_design_pair['pants']['length']['v']
+                            length = min(1.5 * length, new_design['pants']['length']['range'][1])
+                            new_design_pair['pants']['length']['v'] = length
+                            description = 'longer_pants'
+                            break
+
+                        # generic skirts
+                        elif hit == 1 and new_design['meta']['bottom']['v']: 
+                            keys= ['skirt', 'flare-skirt', 'pencil-skirt', 'levels-skirt'] 
+                            for k in keys: 
+                                length = new_design_pair[k]['length']['v']
+                                length = min(1.5 * length, new_design[k]['length']['range'][1])
+                                new_design_pair[k]['length']['v'] = length
+                                description = 'longer_skirt'
+                            break
+
+                        # generic sleeve
+                        elif hit == 2 and new_design['meta']['upper']['v'] and (not new_design['sleeve']['sleeveless']['v']):
+                            length = new_design_pair['sleeve']['length']['v']
+                            length  = min(1.5 * length, new_design['sleeve']['length']['range'][1])
+                            new_design_pair['sleeve']['length']['v'] = length
+                            description = 'longer_sleeve'
+                            break
+
+                        # godet and level skirts 
+                        elif hit == 3 and new_design['meta']['bottom']['v'] in ['GodetSkirt', 'SkirtLevels']:
+                            if new_design['meta']['bottom']['v'] == 'GodetSkirt':
+                                curr_num_inserts = new_design['godet-skirt']['num_inserts']['v']
+                                curr_range = new_design['godet-skirt']['num_inserts']['range']
+                                curr_index = curr_range.index(curr_num_inserts)
+                                curr_range = curr_range[:curr_index] + curr_range[curr_index+1:]
+                                new_inserts = random.choice(curr_range)
+
+                                if curr_num_inserts < new_inserts: 
+                                    description = f'{new_inserts-curr_num_inserts}_more_inserts'
+                                else: 
+                                    description = f'{curr_num_inserts-new_inserts}_less_inserts'
+                                new_design_pair['godet-skirt']['num_inserts']['v'] = new_inserts
+                            else: 
+                                curr_num_levels = new_design['levels-skirt']['num_levels']['v']
+                                curr_range = new_design['levels-skirt']['num_levels']['range']
+                                curr_range = list(range(curr_range[0], curr_range[1]+1))
+                                curr_index = curr_range.index(curr_num_levels)
+                                curr_range = curr_range[:curr_index] + curr_range[curr_index+1:]
+                                new_levels = random.choice(curr_range)
+
+                                if curr_num_levels < new_levels:
+                                    description = f'{new_levels-curr_num_levels}_more_levels'
+                                else:
+                                    description = f'{curr_num_levels-new_levels}_less_levels'
+                                new_design_pair['levels-skirt']['num_levels']['v'] = new_levels
+                            break
+                        
+                        # symmetric 
+                        elif hit == 4 and new_design['meta']['upper']['v'] and new_design['left']['enable_asym']['v']: 
+                            new_design_pair['left']['enable_asym']['v'] = False
+                            description = 'no_asymmetry'
+                            break
+
+                        
+                        # collar 
+                        elif hit == 5 and new_design['meta']['upper']['v'] and not new_design['left']['enable_asym']['v']: 
+                            if random.choice([True, False]) or new_design['collar']['component']['style']['v']: 
+                                curr_collar = new_design['collar']['f_collar']['v']
+                                curr_range = new_design['collar']['f_collar']['range']
+                                curr_index = curr_range.index(curr_collar)
+                                curr_range = curr_range[:curr_index] + curr_range[curr_index+1:]
+                                new_collar = random.choice(curr_range)
+                                new_design_pair['collar']['f_collar']['v'] = new_collar
+                                description = f'switched_to_{new_collar}_from_{curr_collar}'
+                            else: 
+                                new_design_pair['collar']['component']['style']['v'] = 'Hood2Panels'
+                                description = 'includes_hood'
+                            break
+                        
+ 
+                    
+                    name_pair = f'rand_{description}_{index}'
+
+                    # Preliminary checks 
+                    assert_param_combinations(new_design_pair)
+
+                    # On default body
+                    piece_default_pair = MetaGarment(name_pair, default_body, new_design_pair) 
+                    piece_default_pair.assert_total_length()  # Check final length correctnesss
+
+                    # Straight/apart legs pose
+                    def_obj_name = properties['body_default']
+                    if has_pants(new_design_pair):
+                        def_obj_name += '_apart'
+                    default_body.params['body_sample'] = def_obj_name
+
+                    if piece_default_pair.is_self_intersecting():
+                        if verbose:
+                            print(f'{piece_default_pair.name} is self-intersecting!!') 
+                        continue  # Redo the randomization
+                    
+                except: 
+                    print('Error in editing design')
+                    continue
+                    
+                # this means pattern works! 
+                # generate new pattern with same properties but one editing differences
+                
+
+                # let us start with longer sleeves / longer skirt / longer pants: 
+
+
                 # Save samples
                 pattern = _save_sample(piece_default, default_body, new_design, default_sample_data, verbose=verbose)
-                _save_sample(piece_shaped, rand_body, new_design, body_sample_data, verbose=verbose)
+                pattern_pair = _save_sample(piece_default_pair, default_body, new_design_pair, default_sample_data, verbose=verbose)
+                # _save_sample(piece_shaped, rand_body, new_design, body_sample_data, verbose=verbose)
                 
                 stats_utils.count_panels(pattern, props)
                 stats_utils.garment_type(name, new_design, props)
+
+                stats_utils.count_panels(pattern_pair, props)
+                stats_utils.garment_type(name_pair, new_design_pair, props)
 
                 break  # Stop generation
             except KeyboardInterrupt:  # Return immediately with whatever is ready
